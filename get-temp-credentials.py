edd88403e3a3c43cac7e9785c2d2887a19d6a996
hedy kim <47586500+waltzbucks@users.noreply.github.com>
2022-03-13 05:25:08 +0900

    file upload

---
+++ get-temp-credentials.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+import pyotp, time, boto3, json, os, datetime
+import argparse, tempfile
+
+def get_sts_session(**kwargs):
+    session = boto3.session.Session(profile_name=kwargs['profile_name'])
+    return session.client('sts')
+
+def get_token_code(**kwargs):
+    totp = pyotp.TOTP(kwargs['otp_key'])
+    now = datetime.datetime.now()
+    return str(totp.at(now))
+
+def get_credentials(**kwargs):
+
+    STSConnection = get_sts_session(profile_name = kwargs['profile'])
+
+    response = STSConnection.get_session_token(
+        DurationSeconds = kwargs['duration'],
+        SerialNumber = kwargs['mfa_device'],
+        TokenCode = kwargs['token_code']
+    )
+
+    response['Credentials']['Version'] = 1
+    response['Credentials']['Expiration'] = response['Credentials']['Expiration'].isoformat('T')
+    
+    return response['Credentials']
+
+def get_exptime(**kwargs):
+    f = open(kwargs['path'],'r')
+    r = json.loads(f.read())['Expiration']
+    f.close()
+    return datetime.datetime.fromisoformat(r).timestamp()
+    
+
+def main():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device',
+                        required=True,
+                        help="The MFA Device ARN.")
+    parser.add_argument('--duration',
+                        required=False,
+                        type=int,
+                        help="The duration, in seconds, that the temporary "
+                             "credentials should remain valid. Minimum value: "
+                             "900 (15 minutes). Maximum: 129600 (36 hours). "
+                             "Defaults to 43200 (12 hours), or 3600 (one "
+                             "hour).")
+    parser.add_argument('--otpkey',
+                        required=True,
+                        help="Your the secret key of virtual MFA device.")
+    parser.add_argument('--profile',
+                        required=False,
+                        help="If using profiles, specify the name here. The "
+                        "default profile name is 'default'. The value can "
+                        "also be provided via the environment variable "
+                        "'AWS_PROFILE'.")
+    args = parser.parse_args()
+    
+    path = tempfile.gettempdir() + f"/{args.profile}.json"
+    
+    # get profile from param or set the default 
+    if not args.profile:
+        if os.environ.get('AWS_PROFILE'):
+            args.profile = os.environ.get('AWS_PROFILE')
+        else:
+            args.profile = 'default'
+
+    # get duration from param or set default
+    if not args.duration:
+        if os.environ.get('MFA_STS_DURATION'):
+            args.duration = int(os.environ.get('MFA_STS_DURATION'))
+        else:
+            args.duration = 28800 # Time setup to temporary credential for keep using on 8 hour 
+      
+    # temporary credential control through temp file
+    if os.path.exists(path) is False or time.time() > get_exptime(path=path) + args.duration :
+        temp_credential = get_credentials(
+            profile = args.profile,
+            mfa_device = args.device,
+            duration = args.duration,
+            token_code = get_token_code(otp_key = args.otpkey)            
+        )
+        with open(path, 'w', encoding='utf-8') as f:
+            json.dump(temp_credential, f, ensure_ascii=False, indent=4)      
+
+    f = open(path,'r',encoding='utf8')
+    print(f.read())
+    f.close()
+
+if __name__ == "__main__":
+    main()
